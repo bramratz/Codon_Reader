071bd298afb76188950e91ec49e09b5183c2377c
Bram Ratz <bratz@uoguelph.ca>
2020-06-11 15:16:31 -0400

    program, when given a string representing a strand of mRNA, will return the polypeptide chain of amino acids it codes for.

---
+++ readCodons.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Given a string representing a strand of mRNA returns the polypeptide 
+chain of amino acids it codes for.
+Created on Wed Jun 10 11:46:53 2020
+
+@author: bram
+"""
+
+# Import modules needed
+import sys
+
+# Dict of all possible codons 
+
